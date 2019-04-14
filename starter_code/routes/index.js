09f03ebeda4d275070370806438e166f72da3635
Eddie Chung <designered@gmail.com>
2019-04-14 23:24:48 +0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -1,9 +1,25 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Movies = require("../models/Movie");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+// Get movies page
+router.get("/movies", (req, res, next) => {
+  Movies.find({}, (err, docs) => {
+    res.render("movie-list", { docs });
+  }).limit(8);
+});
+
+//Get movie detail page
+router.get("/movie/:id", (req, res, next) => {
+  const movieId = req.params.id;
+  Movies.find({ title: movieId }, (err, doc) => {
+    res.render("movie-details", { doc });
+  });
 });
 
 module.exports = router;
